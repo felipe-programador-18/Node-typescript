c3dfffe2d97a6779e1e717894343b5e20e342e45
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-23 17:10:01 -0300

    just try to organizaed the code

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const router_1 = __importDefault(require("./router/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.listen({ port });
+app.use(body_parser_1.default.json());
+const Getrouter = require("./router/router");
+app.use(router_1.default);
+app.use(Getrouter);
+app.listen(3000);
