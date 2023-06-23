39709c8e6cf5276efe38f9e0cdc3b6ed77c8dada
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-06-23 17:10:09 -0300

    just try to organizaed the code

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
-import express, {Router} from  "express"
+import express from  "express"
 import todosTesting from "./router/router"
 
 import bodyParser from "body-parser"
 
 const app = express()
 app.use(bodyParser.json()) 
+
+const Getrouter = require("./router/router")
+
 app.use(todosTesting)
+app.use(Getrouter)
 
 app.listen(3000)
 
